7dbeecf61d2804a5e16e8926ac79c54eaac9b0c5
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2020-06-24 16:09:47 +0800

    Added ThreeSumClosetSolution 2020-6-24

--- algorithm/src/nums/AddBinarySolution.java
+++ algorithm/src/nums/AddBinarySolution.java
@@ -6,18 +6,26 @@ package nums;
  * 输入为非空字符串且只包含数字 1 和 0
  * 输入： a = "11", b = "1"
  * 输出： "100"
- * @author dell
+ * @author Emily CH
  *
  */
-
 public class AddBinarySolution {
 	
 	public static void main(String[] args) {
-		String a = "1001"; //9
-		String b = "10";//2
-		System.out.println("输出：" + addBinary(a,b)); // 11 : 1011
+		String a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101"; 
+		String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+		System.out.println("输出：" + addBinary(a,b)); 
 	}
-	
+	/**
+	 *   10100000100100110110010000010101111011011001101
+	 * +                        111001100000011011110011
+	 * -------------------------------------------------
+	 * 
+	  *  从右到左逐个相加
+	 * @param a 加数
+	 * @param b 加数
+	 * @return 和的二进制字符串
+	 */	
 	public static String addBinary(String a, String b) {
 		StringBuilder stringBuilder = new StringBuilder();
 		int i = a.length() - 1;
