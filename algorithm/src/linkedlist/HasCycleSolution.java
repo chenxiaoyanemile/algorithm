d63f1310fee7c900e9c42e8451cba1132227ad91
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2020-10-09 17:59:33 +0800

    updated

---
+++ algorithm/src/linkedlist/HasCycleSolution.java
@@ -0,0 +1,35 @@
+package linkedlist;
+
+/**
+ * 141. 环形链表
+ * 给定一个链表，判断链表中是否有环
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环；
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从0 开始）；
+ * 如果 pos 是 -1 ,则在该链表中没有环。
+ * 有环返回 true , 无返回 false.
+ * @author dell
+ *
+ */
+public class HasCycleSolution {
+	
+	public static void main(String args[]) {
+		
+	}
+	
+	/**
+	 * 双指针思维方式
+	 * 
+	 * @param head
+	 * @return
+	 */
+	
+	public boolean hasCycle(ListNode head) {
+		boolean result = false;
+		
+		return result;
+		
+		
+		
+	}
+
+}
