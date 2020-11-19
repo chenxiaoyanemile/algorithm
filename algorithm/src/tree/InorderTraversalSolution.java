75135c491333a743be6b2e9d1ce5ec36358e12bc
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2020-11-19 10:38:48 +0800

    二叉树的中序遍历

--- algorithm/src/tree/InorderTraversalSolution.java
+++ algorithm/src/tree/InorderTraversalSolution.java
@@ -1,11 +1,33 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 94. 二叉树的中序遍历
+ * 给定一个二叉树的根节点 root ，返回他的中序遍历
  * @author dell
  *
  */
 
 public class InorderTraversalSolution {
+	
+	public List<Integer> inorderTraversal(TreeNode root){
+		List<Integer> res = new ArrayList<Integer>();
+		inorder(root,res);
+		return res;
+		
+	}
+	
+	public void inorder(TreeNode root, List<Integer> res) {
+		if(root == null) {
+			return;
+		}
+		
+		inorder(root.left,res);
+		res.add(root.val);
+		inorder(root.right,res);
+		
+	}
 
 }
