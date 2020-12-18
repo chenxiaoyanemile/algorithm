d8d9a958296ca61fe2df6b159d93eb2c649d6935
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2020-12-18 11:42:02 +0800

    连接两个有序链表形成一个有序链表

---
+++ algorithm/src/postgraduate/MergeTwoListsSolution.java
@@ -0,0 +1,33 @@
+package postgraduate;
+/**
+ * 25. 合并两个排序的链表
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * @author dell
+ *
+ */
+
+public class MergeTwoListsSolution {
+	
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode dum = new ListNode(0);
+		ListNode cur = dum;
+		while(l1 != null && l2 != null) {
+			if(l1.val < l2.val) {
+				cur.next = l1;
+				l1 = l1.next;
+			}
+			else {
+				
+				cur.next = l2;
+				l2 = l2.next;
+				
+			}
+			cur = cur.next;
+			
+		}
+		cur.next = l1 !=null ?l1:l2;
+		return dum.next;
+				
+	}
+
+}
