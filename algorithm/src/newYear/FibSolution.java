89a42708f380fddb80bf900b4e6e81912ca9b46f
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2021-01-21 09:29:35 +0800

    updated

--- algorithm/src/newYear/FibSolution.java
+++ algorithm/src/newYear/FibSolution.java
@@ -13,7 +13,18 @@ package newYear;
 public class FibSolution {
 	
 	public int fib(int n) {
-		
+		if(n < 2) {
+			return n;		
+		}
+		int p = 0;
+		int q = 0;
+		int r = 1;
+		for(int i = 2; i <= n; ++i) {
+			p = q;
+			q = r;
+			r = p+q;			
+		}
+		return r;
 		
 	}
 
