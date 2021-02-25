9303043eff4c385af83f1b86de387661bab29dab
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2021-02-25 10:44:19 +0800

    更新

--- algorithm/src/newYear/FlipAndInvertImageSolution.java
+++ algorithm/src/newYear/FlipAndInvertImageSolution.java
@@ -1,5 +1,12 @@
 package newYear;
 
+/**
+ * 832. 翻转图像
+ * 给定一个二进制矩阵 A，
+ * @author dell
+ *
+ */
+
 public class FlipAndInvertImageSolution {
 
 }
--- algorithm/src/newYear/TransposeSolution.java
+++ algorithm/src/newYear/TransposeSolution.java
@@ -1,5 +1,38 @@
 package newYear;
 
+/**
+ * 867.转置矩阵
+ * 给你一个二维整数数组 matrix, 返回 matrix 的转置矩阵
+ * 矩阵的转置是指将矩阵的主对角线翻转，交换矩阵的行索引和列索引
+ * 
+ * 实例：
+ * 输入： matrix ={{1,2,3},{4,5,6},{7,8,9}}
+ * 输出：{{1,4,7},{2,5,8},{3,6,9}}
+ * @author dell
+ *
+ */
+
 public class TransposeSolution {
+	
+	public static void main(String[] args) 
+	{
+		int[][] matrix ={{1,2,3},{4,5,6},{7,8,9}};
+	}
+	
+	public int[][] transpose(int[][] matrix)
+	{
+		int M = matrix.length;
+		int N = matrix[0].length;
+		int[][] result = new int[N][M];
+		for(int i = 0; i < M; i++) 
+		{
+			for(int j = 0; j < N;j++)
+			{
+				result[j][i] = matrix[i][j];
+			}
+		}
+		return result;
+		
+	}
 
 }
