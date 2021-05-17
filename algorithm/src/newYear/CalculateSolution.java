e3995d3af706eccd5d4b3b9b3271414f39027300
chenxiaoyanemile <chenxiaoyanemile@gmail.com>
2021-05-17 20:32:36 +0800

    updtae

--- algorithm/src/newYear/CalculateSolution.java
+++ algorithm/src/newYear/CalculateSolution.java
@@ -1,5 +1,7 @@
 package newYear;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,9 +33,101 @@ public class CalculateSolution {
 		put('^',3);
 	}};
 	
+	/**
+	 * 双栈解法
+	 * 对于任何表达式而言，我们都可以使用两个栈 nums 和 ops  解决：
+	 * nums:存放所有的数字
+	 * ops: 存放所有的数字以外的操作
+	 * @param s
+	 * @return
+	 */
 	public int calculate(String s) {
+		// 将所有的空格去掉，并将 (- 替换成 (0-, (+ 替换成 (0+
+		s = s.replaceAll(" ", "");
+		s = s.replaceAll("\\(-", "(0-");
+		s = s.replaceAll("\\(\\+", "(0+");
+		char[] cs = s.toCharArray();
+		int n = s.length();
+		// 存放所有的数字
+		Deque<Integer> nums = new ArrayDeque<>();
+		// 为了防止第一个数是负数，先往 nums 加个 0
+		nums.addLast(0);
+		Deque<Character> ops= new ArrayDeque<>();
+		for(int i = 0; i < n; i++) {
+			char c = cs[i];
+			if(c == '(') 
+			{
+				ops.addLast(c);
+			}
+			else if(c == ')') 
+			{
+				// 计算到最近一个左括号为止
+				while(!ops.isEmpty()) 
+				{
+					if(ops.peekLast() != '(') 
+					{
+						calc(nums,ops);
+					}
+					else 
+					{
+						ops.pollLast();
+						break;
+					}
+				}				
+			}
+			else 
+			{
+				if(isNumber(c)) 
+				{
+					int u = 0;
+					int j = i;
+					while(j < n && isNumber(cs[j])) u = u * 10 + (cs[j++] -'0');
+					nums.addLast(u);
+					i = j-1;
+				}
+				else 
+				{
+					// 有一个新操作要入栈时，先把栈内可以算的都算了
+					// 只有满足 [栈内运算符] 比 [当前运算符] 优先级高 / 同等，才进行运算
+					while(!ops.isEmpty() && ops.peekLast() != '(') 
+					{
+						char prev = ops.peekLast();
+						if(map.get(prev) >= map.get(c)) {
+							calc(nums,ops);
+						}
+						else {
+							break;
+						}
+					}
+					ops.addLast(c);
+				}
+			}
+			
+			
+		}
+		while(!ops.isEmpty())  calc(nums,ops);
+		return nums.peekLast();
 		
+	}
+	
+	public void calc(Deque<Integer> nums, Deque<Character> ops) {
+		if(nums.isEmpty() || nums.size() < 2) return;
+		if(ops.isEmpty()) return;
+		int b = nums.pollLast(), a = nums.pollLast();
+		char op = ops.pollLast();
+		int ans = 0;
+		if(op == '+') ans = a+b;
+		else if(op == '-') ans = a-b;
+		else if(op == '*') ans = a*b;
+		else if(op == '/') ans = a/b;
+		else if(op == '^') ans = (int) Math.pow(a, b);
+		else if(op == '%') ans = a%b;
+		nums.addLast(ans);
 		
+	}
+	
+	boolean isNumber(char c) {
+		return Character.isDigit(c);
 	}
 	
 
